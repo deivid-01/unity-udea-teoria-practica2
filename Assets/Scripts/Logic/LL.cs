d98e97bd31307e36a2104182062a9d67847c26fc
David <Torres>
2020-05-28 21:25:11 -0500

    UI and controller done

---
+++ Assets/Scripts/Logic/LL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LL
+{
+    public static bool Belong ( string grammar )
+    {
+        //Checkgrammar
+        throw new NotImplementedException ( "Belong no se ha implementado" );
+
+    }
+
+    public LL ()
+    {
+        //Build grammmar
+    }
+
+    public bool ValidateHilera ( string hilera )
+    {
+        //ValidateHilera
+        throw new NotImplementedException ();
+    }
+}
---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,88 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    #region Singlenton
+    public static UI instance;
+
+    private void Awake ()
+    {
+        instance = this;
+    }
+    #endregion
+
+
+    public GameObject windowGrammar;
+    public GameObject windowConfirmationGrammar;
+
+    public GameObject windowHilera;
+    public GameObject windowConfirmationHilera;
+
+
+    public GameObject actualWindow;
+
+    public GameObject windowResultGrammar;
+    public GameObject windowResultHilera;
+    public Text resultGrammar;
+    public Text resultHilera;
+    private void Start ()
+    {
+        actualWindow = windowGrammar;
+    }
+    public void ReadGrammar ( InputField inputGrammar )
+    {
+        
+      
+        Controller.instance.CheckGrammar ( inputGrammar.text );
+    }
+
+    public void ReadHilera ( InputField inputHilera )
+    {
+        Controller.instance.CheckHilera ( inputHilera.text );
+    }
+
+    public void GetAnswerGrammar ( bool ans )
+    {
+
+        if ( ans )
+        {
+            Enable ( windowGrammar );
+            return;
+        }
+        Application.Quit (); //exit
+    }
+
+    public void GetAnswerHilera ( bool ans )
+    {
+
+        if ( ans )
+        {
+            Enable ( windowHilera );
+            return;
+        }
+        Enable ( windowConfirmationGrammar );
+    }
+
+    public void ShowResult ( GameObject window,Text result,string res ) {
+        Enable ( window );
+
+        result.text = res;
+    }
+  
+
+    public void ShowWindow ( GameObject window )
+    {
+        Enable ( window );
+    }
+
+    public void Enable ( GameObject next )
+    {
+        actualWindow.SetActive ( false );
+        next.SetActive ( true );
+        actualWindow = next;
+    }
+}
